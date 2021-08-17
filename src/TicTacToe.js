77adffc0e5f1ff5aa058edec4188a626c41fce6d
Thalles Carneiro <thallescarneiro@gmail.com>
2021-08-16 23:44:04 -0300

    Exercicio - React Hooks Refactoring - Bônus: Ex. 3

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GameBoard from './GameBoard';
 
-class TicTacToe extends React.Component {
-  static victoryArchivedInLine(gameBoard) {
+const TicTacToe = () => {
+  const [activePlayer, setActivePlayer] = useState(1);
+  const [gameBoard, setGameBoard] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+  const toggleActivePlayer = () => setActivePlayer((activePlayer === 1) ? 2 : 1)
+
+  const updateState = (cellClicked) => {
+    if (gameBoard[cellClicked] !== 0) return false;
+
+    const newGameBoard = [...gameBoard];
+    newGameBoard[cellClicked] = activePlayer;
+    
+    setGameBoard(newGameBoard);
+    toggleActivePlayer();
+  };
+
+  const victoryArchivedInLine = (gameBoard) => {
     for (let i = 0; i <= 6; i += 3) {
       if (
         gameBoard[i] === gameBoard[i + 1]
@@ -11,9 +26,9 @@ class TicTacToe extends React.Component {
       ) return gameBoard[i];
     }
     return false;
-  }
+  };
 
-  static victoryArchivedInColumn(gameBoard) {
+  const victoryArchivedInColumn = (gameBoard) => {
     for (let i = 0; i <= 2; i += 1) {
       if (
         gameBoard[i] === gameBoard[i + 3]
@@ -22,9 +37,9 @@ class TicTacToe extends React.Component {
       ) return gameBoard[i];
     }
     return false;
-  }
+  };
 
-  static victoryArchivedInDiagonals(gameBoard) {
+  const victoryArchivedInDiagonals = (gameBoard) => {
     if (gameBoard[4] === 0) return false;
     if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) {
       return gameBoard[0];
@@ -33,97 +48,52 @@ class TicTacToe extends React.Component {
       return gameBoard[2];
     }
     return false;
-  }
+  };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      activePlayer: 1,
-      gameBoard: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    };
+  const resetGame = () => {
+    setActivePlayer(1);
+    setGameBoard([0, 0, 0, 0, 0, 0, 0, 0, 0])
+  };
 
-    this.updateState = this.updateState.bind(this);
-    this.resetGame = this.resetGame.bind(this);
-    this.renderButton = this.renderButton.bind(this);
-  }
+  const victoryArchieved = () => (
+    victoryArchivedInLine(gameBoard)
+    || victoryArchivedInColumn(gameBoard)
+    || victoryArchivedInDiagonals(gameBoard)
+  );
 
-  resetGame() {
-    this.setState({
-      activePlayer: 1,
-      gameBoard: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    });
-  }
+  const renderButton = () => (
+    <button
+      type="button"
+      onClick={resetGame}
+      data-testid="restart-button"
+    >
+      Recomeçar Jogo
+    </button>
+  );
 
-  toggleActivePlayer() {
-    const { activePlayer } = this.state;
-    if (activePlayer === 1) return 2;
-    return 1;
-  }
-
-  updateState(cellClicked) {
-    this.setState((state) => {
-      const newState = [...state.gameBoard];
-      let newActivePlayer = state.activePlayer;
-
-      if (state.gameBoard[cellClicked] === 0) {
-        newState[cellClicked] = state.activePlayer;
-        newActivePlayer = this.toggleActivePlayer();
-      } else newState[cellClicked] = state.gameBoard[cellClicked];
-
-      return {
-        activePlayer: newActivePlayer,
-        gameBoard: newState,
-      };
-    });
-  }
-
-  victoryArchieved() {
-    const { gameBoard } = this.state;
-
-    return (
-      TicTacToe.victoryArchivedInLine(gameBoard)
-      || TicTacToe.victoryArchivedInColumn(gameBoard)
-      || TicTacToe.victoryArchivedInDiagonals(gameBoard)
-    );
-  }
-
-  renderButton() {
-    return (
-      <button
-        type="button"
-        onClick={this.resetGame}
-        data-testid="restart-button"
-      >
-        Recomeçar Jogo
-      </button>
-    );
-  }
-
-  render() {
-    const { gameBoard } = this.state;
-    const win = this.victoryArchieved();
-    if (!gameBoard.includes(0) && !win) {
-      return (
-        <>
-          {this.renderButton()}
-          <h1>Empate</h1>
-        </>
-      );
-    }
+  const win = victoryArchieved();
+  if (!gameBoard.includes(0) && !win) {
     return (
       <>
-        {this.renderButton()}
-        {(!win)
-          ? (
-            <GameBoard
-              gameState={gameBoard}
-              updateGame={this.updateState}
-            />
-          )
-          : <h1>{`Player ${win === 2 ? 'O' : 'X'} Ganhou`}</h1>}
+        {renderButton()}
+        <h1>Empate</h1>
       </>
     );
   }
+  return (
+    <>
+      {renderButton()}
+      {(!win)
+        ? (
+          <GameBoard
+            gameState={gameBoard}
+            updateGame={updateState}
+          />
+        )
+        : <h1>{`Player ${win === 2 ? 'O' : 'X'} Ganhou`}</h1>}
+    </>
+  );
+
 }
 
 export default TicTacToe;
